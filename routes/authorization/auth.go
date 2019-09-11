a06cedc676766b7aaa3485ea090ccd4553907cce
liyiwen <liyiwen@huoyunren.com>
2019-09-11 22:18:05 +0800

    teleport plugins

--- routes/authorization/auth.go
+++ routes/authorization/auth.go
@@ -2,6 +2,6 @@ package authorization
 
 import "github.com/henrylee2cn/teleport"
 
-func (a *Authorization) Auth(key []byte) (token []byte, status *tp.Status) {
+func (a *Authorization) Auth(key *[]byte) (token []byte, status *tp.Status) {
 	return
 }
