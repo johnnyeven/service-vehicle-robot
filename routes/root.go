3489c786b4db9e95dbce9e95dec4629fdece47f8
liyiwen <liyiwen@huoyunren.com>
2019-09-12 22:57:24 +0800

    teleport plugins

--- routes/root.go
+++ routes/root.go
@@ -3,12 +3,14 @@ package routes
 import (
 	"github.com/johnnyeven/libtools/courier"
 	"github.com/johnnyeven/libtools/courier/swagger"
+	"github.com/johnnyeven/service-vehicle-robot/routes/nodes"
 )
 
 var RootRouter = courier.NewRouter(GroupRoot{})
 
 func init() {
 	RootRouter.Register(swagger.SwaggerRouter)
+	RootRouter.Register(nodes.Router)
 }
 
 type GroupRoot struct {
