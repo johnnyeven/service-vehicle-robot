fab07e63396be955fb8185a9eade968a09c1630b
liyiwen <liyiwen@huoyunren.com>
2019-10-16 19:28:20 +0800

    REMOVE camera_mode configuration

--- global/robot_configuration.go
+++ global/robot_configuration.go
@@ -1,11 +1,6 @@
 package global
 
-import (
-	"github.com/johnnyeven/service-vehicle-robot/constants/types"
-)
-
 type RobotConfiguration struct {
-	CameraMode types.CameraMode `json:"cameraMode"`
 }
 
 func (c RobotConfiguration) Init() {
--- routes/detection/object.go
+++ routes/detection/object.go
@@ -3,20 +3,13 @@ package detection
 import (
 	"github.com/henrylee2cn/teleport"
 	"github.com/johnnyeven/service-vehicle-robot/constants/errors"
-	"github.com/johnnyeven/service-vehicle-robot/constants/types"
 	"github.com/johnnyeven/service-vehicle-robot/global"
 	"github.com/johnnyeven/service-vehicle-robot/modules/models"
 )
 
 // 物体检测
 func (r *Detection) Object(req *models.CameraRequest) ([]models.DetectivedObject, *tp.Status) {
-	data := make([]models.DetectivedObject, 0)
-	var err error
-	if global.Config.RobotConfiguration.CameraMode == types.CAMERA_MODE__NORMAL {
-		return data, nil
-	}
-
-	data, err = models.DetectiveObject(req, global.Config.COCOModel)
+	data, err := models.DetectiveObject(req, global.Config.COCOModel)
 	if err != nil {
 		statusErr := errors.InternalError.StatusError().WithDesc(err.Error())
 		return nil, tp.NewStatus(int32(statusErr.Code), statusErr.Desc, err)
